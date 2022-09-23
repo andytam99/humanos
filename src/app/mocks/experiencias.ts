c43e40c9e641ef30ad9b5fcc1bc34bb8201014a2
Andy Tamayo <andresjt@icloud.com>
2022-09-23 16:46:05 -0600

    img optimization

--- src/app/mocks/experiencias.ts
+++ src/app/mocks/experiencias.ts
@@ -2,47 +2,47 @@ import { CarSlide } from '../interfaces/carousel';
 
 export const Experiencias: CarSlide[] = [
   {
-    img: '/assets/images/yoga.jpeg',
+    img: '/assets/images/yoga.jpg',
     title: 'Yoga',
     text: 'Meditación y balance corporal para el desarrollo de la templanza y el enfoque en las personas, en un tiempo en que se vive una vida acelerada, ansiosa, altamente activa y un constante necesidad de distracciones poco saludables.',
   },
   {
-    img: '/assets/images/foraging.jpeg',
+    img: '/assets/images/foraging.jpg',
     title: 'Alimentación',
     text: 'Culturizar de dónde viene lo que comemos, dónde encontrarlo y cómo cultivarlo, qué debemos y qué no debemos comer, hábitos saludables de alimentación.',
   },
   {
-    img: '/assets/images/leer.jpeg',
+    img: '/assets/images/leer.jpg',
     title: 'Crecimiento Personal',
     text: 'Estrechar de nuevo la conexión entre la sabiduría antigua, restablecer hábitos saludables y remediar comportamientos perjudiciales.',
   },
   {
-    img: '/assets/images/artesanal.jpeg',
+    img: '/assets/images/artesanal.jpg',
     title: 'Artes Plásticas',
     text: 'Dibujo, pintura, artesanías de barro, tejido y otras actividades manuales simples con materiales y técnicas naturales para el uso propio.',
   },
   {
-    img: '/assets/images/music.jpeg',
+    img: '/assets/images/music.jpg',
     title: 'Música',
     text: 'Hábito saludable para reducir el estrés y fomentar la expresión en las personas. Armonización de la personalidad de las personas. Música que busque embellecer a la gente incitándolos al baile y al canto, en contraste de incitar a la gente a admirar el ego del músico y su talento.',
   },
   {
-    img: '/assets/images/dancing.jpeg',
+    img: '/assets/images/dancing.jpg',
     title: 'Baile',
     text: 'Actividades que permitan a las personas relacionarse y embellecer la música con armonía física, con cultura y mutualismo, en contraste de aumentar el ego de las personas con talento y acrobacias.',
   },
   {
-    img: '/assets/images/martial.jpeg',
+    img: '/assets/images/martial.jpg',
     title: 'Artes Marciales',
     text: 'Actividades físicas para un mejor manejo del cuerpo, reducir ansiedad y desviar el estrés.',
   },
   {
-    img: '/assets/images/planting.jpeg',
+    img: '/assets/images/planting.jpg',
     title: 'Protección Forestal',
     text: 'Estrechar la conexión entre las personas y la naturaleza con trabajos de reforestación, educación y observación de vida silvestre y de botánica.',
   },
   {
-    img: '/assets/images/hiking.jpeg',
+    img: '/assets/images/hiking.jpg',
     title: 'Senderismo',
     text: 'Actividades recreativas como remplazo de una caminata por la ciudad, con una por la naturaleza.',
   },
--- src/app/mocks/servicios.ts
+++ src/app/mocks/servicios.ts
@@ -2,17 +2,17 @@ import { CarSlide } from '../interfaces/carousel';
 
 export const Servicios: CarSlide[] = [
   {
-    img: '/assets/images/yoga.jpeg',
+    img: '/assets/images/yoga.jpg',
     title: 'Yoga',
     text: 'Experiencia enseñando variedades de estilos y clases de Yoga. Con habilidad para interpretar y comunicar teorías, filosofías y prácticas del Yoga.',
   },
   {
-    img: '/assets/images/foraging.jpeg',
+    img: '/assets/images/foraging.jpg',
     title: 'Alimentación',
     text: 'Título universitario en áreas relacionadas a la Nutrición o Botánica o conocimiento y experiencia demostrable de comestibles, cultivo, supervivencia y conservación de vida silvestre.',
   },
   {
-    img: '/assets/images/running.jpeg',
+    img: '/assets/images/running.jpg',
     title: 'Ejercicios',
     text: 'Con experiencia demostrable en el entrenamiento y cuidado del cuerpo, preferiblemente adiestrado en ejercicios con el peso del cuerpo o con un requerimiento mínimo de equipo para realizarlos.',
   },
@@ -22,17 +22,17 @@ export const Servicios: CarSlide[] = [
     text: 'Conocimiento demostrable de artes manuales con materiales naturales, de bajo impacto ambiental, así como de métodos culturales tradicionales y naturales para la obra de útiles.',
   },
   {
-    img: '/assets/images/music.jpeg',
+    img: '/assets/images/music.jpg',
     title: 'Música',
     text: 'Con conocimiento de instrumentos portables y acústicos, con instrumento propio, con conocimiento de estilos de música tradicional latina, y con capacidad de demostrar su talento con el instrumento.',
   },
   {
-    img: '/assets/images/dancing.jpeg',
+    img: '/assets/images/dancing.jpg',
     title: 'Baile',
     text: 'Con conocimiento demostrable de bailes culturales y tradicionales latinos, con excelente capacidad comunicativa y habilidad para enseñar y animar a la gente.',
   },
   {
-    img: '/assets/images/martial.jpeg',
+    img: '/assets/images/martial.jpg',
     title: 'Artes Marciales',
     text: 'Con experiencia demostrable en alguno de varios tipos de artes marciales que requieran un mínimo de equipo para practicar, enseñar o entrenar.',
   }
